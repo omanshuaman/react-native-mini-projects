3fa02502842453aa4d3aeb04513616e260351527
Omanshuaman <omanshuaman@gmail.com>
2025-06-24 12:45:22 +0530

    init commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,57 +1,5 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import "react-native-reanimated";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistor, store } from "../store/store.js";
-
-import { useColorScheme } from "@/hooks/useColorScheme";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ActivityIndicator } from "react-native";
-
 import { Stack } from "expo-router";
-const client = new QueryClient();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
-  return (
-    <QueryClientProvider client={client}>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <Provider store={store}>
-          <PersistGate loading={<ActivityIndicator />} persistor={persistor}>
-            <Stack
-              screenOptions={{
-                headerShown: false,
-              }}>
-              <Stack.Screen
-                name="(tabs)"
-                options={{
-                  title: "overview",
-                }}
-              />
-              <Stack.Screen
-                name="+not-found"
-                options={{
-                  title: "overview",
-                }}
-              />
-            </Stack>
-          </PersistGate>
-        </Provider>
-      </ThemeProvider>
-    </QueryClientProvider>
-  );
+  return <Stack />;
 }
